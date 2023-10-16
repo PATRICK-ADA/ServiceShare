5aa39b60f37fe993be825f66a6edd7b11dabc9db
PATRICK-ADA <donpat646@gmail.com>
2023-10-16 00:40:30 +0000

    Added a Model

---
+++ ServiceShare.proj/User/User.Domain/UserModel.cs
@@ -0,0 +1,17 @@
+using System.Runtime.InteropServices;
+using System;
+namespace Model
+{
+public class User
+{
+	public string UserId {get; set;} = Guid.NewGuid().Tostring();
+	public string UserName {get; set;} = string.Empty;
+	public string? EmailAddress {get; set;} = string.Empty;
+	public string ServiceTypeId {get; set;} = string.Empty;
+	public string SubsTypeId {get; set;} = string.Empty;
+	
+}
+
+
+
+}
